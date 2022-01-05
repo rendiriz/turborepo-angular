8c45cd0298993f843b70fb78197a3ead43f24cde
Rendi Rizki <rendirizkir@gmail.com>
2022-01-05 18:25:31 +0700

    test: husky

--- packages/config/eslint-angular.js
+++ packages/config/eslint-angular.js
@@ -7,9 +7,9 @@ module.exports = {
       //   createDefaultProgram: true,
       // },
       extends: [
+        'eslint:recommended',
         'plugin:@angular-eslint/recommended',
         'plugin:@angular-eslint/template/process-inline-templates',
-        'eslint:recommended',
         'plugin:@typescript-eslint/recommended',
         'airbnb',
         'airbnb-typescript',
